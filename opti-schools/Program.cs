c1df5af78b55ca139e724049ea16cb4b3807b31d
OS-System256 <ArtemLemans@gmail.com>
2023-11-11 15:40:34 +0300

    tel

---
+++ opti-schools/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace opti_schools
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("fly");
+            var a = new Home_work();
+            Console.WriteLine(a);
+        }
+    }
+}
---
+++ opti-schools/home-work.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace opti_schools
+{
+    class Home_work
+    {
+        public Home_work()
+        {
+            Console.Write("hool");
+            string a;
+            a = Console.ReadLine();
+            Console.WriteLine(a);
+        }
+    }
+}
